4ce0a2c11dde869900baa7a5a9a1304016789644
Virtuoel <Virtuoel@users.noreply.github.com>
2022-12-26 07:09:30 +0300

    Fix version util range checker

--- src/main/java/virtuoel/no_fog/util/VersionUtils.java
+++ src/main/java/virtuoel/no_fog/util/VersionUtils.java
@@ -23,7 +23,8 @@ public class VersionUtils
 			{1, 16, 5},
 			{1, 17, 1},
 			{1, 18, 2},
-			{1, 19, 2},
+			{1, 19, 3},
+			{1, 20, 0},
 		};
 		
 		final String prefix = ".compat%s%s";
@@ -46,7 +47,7 @@ public class VersionUtils
 			VERSION_PREDICATES.add(n -> n.contains(predicateMinorZero) && (VersionUtils.MINOR != minor || VersionUtils.PATCH != 0));
 			VERSION_PREDICATES.add(n -> n.contains(predicateMinorZeroMinus) && (VersionUtils.MINOR > minor || (VersionUtils.MINOR == minor && VersionUtils.PATCH > 0)));
 			
-			final int maxPatch = range[1];
+			final int maxPatch = range[2];
 			
 			for (int i = 1; i <= maxPatch; i++)
 			{
