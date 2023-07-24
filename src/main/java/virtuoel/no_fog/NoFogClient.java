7548ff4bc7b9866a6b640f5881ce9ea32c788e60
Terminal-Access <me@leewickham.co.uk>
2023-07-24 16:05:57 +0100

    Correctly declare to the FML that the mod is client side only and ignores server side.
    This fixes the bug where the server will report an invalid mod list with this mod loaded on the client.

--- src/main/java/virtuoel/no_fog/NoFogClient.java
+++ src/main/java/virtuoel/no_fog/NoFogClient.java
@@ -2,12 +2,17 @@ package virtuoel.no_fog;
 
 import java.util.function.Supplier;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+
 import virtuoel.no_fog.api.NoFogConfig;
 import virtuoel.no_fog.util.AutoConfigUtils;
 import virtuoel.no_fog.util.DummyNoFogConfig;
@@ -31,6 +36,9 @@ public class NoFogClient
 	
 	public NoFogClient()
 	{
+		//Correctly tell FML we are a Client Side only Mod, and we are ignoring server side.     
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
 		if (CONFIGS_LOADED)
 		{
 			AutoConfigUtils.initialize();
